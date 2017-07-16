3380238c90bea0de6c25ce15f5c58a53c00c502c
erolle <edwin.rolle@qudosoft.de>
2017-07-16 13:03:46 +0200

    Added the iml file to ignored files in git, made my error handler non public

--- src/main/java/de/eddykaya/openweather/client/HttpClientErrorHandler.java
+++ src/main/java/de/eddykaya/openweather/client/HttpClientErrorHandler.java
@@ -8,10 +8,8 @@ import org.springframework.web.client.ResponseErrorHandler;
 
 import de.eddykaya.openweather.client.exceptions.ResourceNotFoundException;
 
-/**
- *
- */
-public class HttpClientErrorHandler implements ResponseErrorHandler {
+class HttpClientErrorHandler implements ResponseErrorHandler {
+
 	@Override
 	public boolean hasError(final ClientHttpResponse response) throws IOException {
 		return response.getStatusCode().equals(HttpStatus.NOT_FOUND);
